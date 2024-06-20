f1d268dcdea1e272e9668209e8c9672256d162af
Sultan Muhammad Dhavi <sultanmuhammaddhavi@gmail.com>
2024-06-20 09:31:06 +0700

    menambahkan product

--- src/components/products/product.tsx
+++ src/components/products/product.tsx
@@ -46,6 +46,34 @@ const Products = () => {
                 "Sunbather expanded on the group's dynamic 2011 debut and doubled down on the wall-of-sound arrangements heavily influenced by shoegaze, juxtaposed with opaque screaming, breakdowns and overall song structures typically found on black metal recordings.",
               price: "$24.99",
             },
+            {
+              img: "https://pbs.twimg.com/media/GJhkZKWXkAAvpvo.jpg:large",
+              title: "Mammoth Grinder 'Cosmic Crypt' LP",
+              description:
+                "Underground extreme metal trio MAMMOTH GRINDER return after five long years with their fourth full-length Cosmic Crypt, a non-stop, meteoric force of aggression and mayhem.",
+              price: "$24.40",
+            },
+            {
+              img: "/images/gatecreeper-1.png",
+              title: "Gatecreeper 'Dark Superstition' LP",
+              description:
+                "Dark Superstition was recorded at God City Studios in Salem, Massachusetts, with Converge guitarist Kurt Ballou, who also mixed the album. The record is a direct result of Gatecreeper's experience, musical refinement, and commitment to death metal.",
+              price: "$32.19",
+            },
+            {
+              img: "/images/agri-1.png",
+              title: "Agriculture 'Agriculture' LP",
+              description:
+                "For something to be 'ecstatic,' the feelings and emotions it evokes must transcend what we tend to experience most regularly in our lives. Ecstatic joy isn't just happiness; it's a feeling of jubilation which impacts us emotionally and in a metaphysical, arguably spiritual sense too.",
+              price: "$24.99",
+            },
+            {
+              img: "https://images.tokopedia.net/img/cache/700/VqbcmM/2023/2/17/c1e0b638-6468-47b7-ac73-e3b10f82cecd.jpg",
+              title: "Avhath 'FELO DE SE/HALLOWED GROUND' 7\"",
+              description:
+                "Disaster Records presents a special hazy blood red 7″ vinyl limited to 222pcs, seminal maxi-single from Avhath released in 2021 featuring an award-winning single FELO DE SE and the furious HALLOWED GROUND, both produced by Alyuadi Febryansyah.",
+              price: "$16.81",
+            },
           ].map((product, index) => (
             <div
               key={index}
